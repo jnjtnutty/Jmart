014a659d3589cd75adffe951660bedaaf9edadf4
jnjtnutty <pattaraporngc@gmail.com>
2021-01-06 23:52:07 +0700

    Tab menu

--- app/src/main/java/com/example/android/jmart/MainActivity.kt
+++ app/src/main/java/com/example/android/jmart/MainActivity.kt
@@ -1,18 +1,41 @@
 package com.example.android.jmart
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.navigation.findNavController
-import com.example.android.jmart.databinding.ActivityMainBinding
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.android.jmart.databinding.JmartMainBinding
+import kotlinx.android.synthetic.main.jmart_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: JmartMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = JmartMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.buttonOne.text="Number One"
+
+        val productFragment = ProductFragment()
+        val promotionFragment = PromotionFragment()
+        val videoFragment = VideoFragment()
+
+        makeCurrentFragment(productFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener { it:MenuItem->
+            when(it.itemId){
+                R.id.ic_product -> makeCurrentFragment(productFragment)
+                R.id.ic_promotion -> makeCurrentFragment(promotionFragment)
+                R.id.ic_video -> makeCurrentFragment(videoFragment)
+            }
+            true
+        }
      }
+
+    private fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
+    }
 }
